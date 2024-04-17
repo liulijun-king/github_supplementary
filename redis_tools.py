6d785bf2fc3d8f7be7ccc8795e06d54e75ed8e87
liulijun <1143421302@qq.com>
2024-04-17 14:49:15 +0800

    修改到5亿id值

--- redis_tools.py
+++ redis_tools.py
@@ -23,8 +23,7 @@ def add_id():
         logger.info(f"redis数据库长度为{key_len}")
         count = 0
         if key_len < 1000000:
-            # max_id = redis_conn.lindex(id_key, -1)
-            max_id = 500000000
+            max_id = redis_conn.lindex(id_key, -1)
             logger.info(f"当前序列末尾为：{max_id}")
             with redis_conn.pipeline() as pipe:
                 for _ in range(max_id, max_id - 10000000, -1):
